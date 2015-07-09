efad92a1357c20b66163d628da18405b14a93338
Matthias L. Jugel <matthias.jugel@streamdrill.com>
2015-07-09 08:04:24 +0200

    added comment about arduiono sdk version

--- arduino.mk
+++ arduino.mk
@@ -1,6 +1,7 @@
 # http://hardwarefun.com/tutorials/compiling-arduino-sketches-using-makefile
+# currently works with Arduino SDK 1.6.4
 
-#BOARD_TAG=uno
+BOARD_TAG=uno
 ARDUINO_PORT=/dev/cu.SLAB_USBtoUART
 
 include /usr/local/opt/arduino-mk/Arduino.mk
